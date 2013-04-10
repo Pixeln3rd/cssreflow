1626edfdbabac016164b6904dc2dc4bd94da21f0
Michael Kaminski <hello@michaelkaminski.me>
2013-04-10 14:57:46 -0500

    First commit
    First commit cssreflow project repo

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>CSS Reflow | Reflow Visualizations</title>
+<meta name="keywords" content="css, css reflow, reflow, browser reflow, DOM, DOM rendering, css reflow visualization, reflow visualization, web developers, web browser, video, css video">
+<meta name="description" content="CSS Reflow visualization videos made using a modified build of Firefox 11">
+<meta name="google-site-verification" content="MpxgVBzK2d0wOQgkF2ul-gohYebhPXrhI_3YhOu8NEs" />
+
+<!-- hey there, thanks for checkin out my source code. questions or comments hit me up on twitter @pixeln3rd -->
+
+
+<link rel="icon" type="image/png" href="/favicon.png">
+
+
+<link href='assets/css/style.css' rel='stylesheet' type='text/css'>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="container">
+  <div class="section" id="section1">
+  
+    <div class="content">
+      <div class="left">
+     <img src="assets/images/logo.png" width="228" height="215" alt="Css Reflow" class="logo">
+     <div class="ffWrap">
+      
+      <h1 class="headline" id="infheader">CSS Reflow visualizations made using a modified build of Firefox 11</h2>
+      <div class="subheadline" id="bigguys">See how the browser builds your site.</div>
+      </div>
+      <!-- end .left --></div>
+    <div class="right">
+      <div class="vid"><iframe width="440" height="253" src="http://www.youtube.com/embed/TG4qko9JUAE?rel=0" frameborder="0" allowfullscreen></iframe></div>
+      
+      <div class="stack">Wanna see how your website looks?</div>
+      <!-- end .right --></div>
+    <div class="priceBox">
+      <div class="submitWrap">
+      <div class="pricetext"><span class="headline" id="text600">Submit your URL</span></div>
+      <div class="L1button"><a href="#section2" id="yelbutton1"><span>How It Works</span></a></div>
+     <br class="clearfloat"> </div>
+    </div>
+    <div class="L1footer">
+    <h2 class="subheadline" id="connect">What is Browser Reflow?</h2>
+    
+    <p class="text">"Reflow is the name of the web browser process for re-calculating the positions and geometries of elements in the document, for the purpose of re-rendering part or all of the document. Because reflow is a user-blocking operation in the browser, it is useful for developers to understand how to improve reflow time and also to understand the effects of various document properties (DOM depth, CSS rule efficiency, different types of style changes) on reflow time. Sometimes reflowing a single element in the document may require reflowing its parent elements and also any elements which follow it."<br/>
+      <a href="https://developers.google.com/speed/articles/reflow" title="Minimizing Browser Reflow" target="_blank" class="googLink">- Google Developer Blog</a></p>
+    
+    </div>
+    <br class="clearfloat"><!-- end .content --></div>
+    
+  <!-- end #section1 --></div> 
+
+  <div class="section" id="section2"> 
+  
+          <div class="content"><div class="headline" id="L2headline">How It Works</div>
+        <div class="subheadline" id="L2subheadline">Submit Your URL</div>
+        <p class="text">Right now we are accepting limited submissions while our project is in beta testing. Eventually, this app will analyze your url, then generate an .avi file to download.  For the time being, we are manually processing all videos, which takes some considerable time. Submit your URL and we will try and generate a video for you,  although  all videos will be hosted on our youtube channel, feel free to link and embed at your leisure!</p><br/>
+        <p class="text">WE DO NOT SPAM. We only ask for your email to notify you that a video has been uploaded.</p>
+       
+          <!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="http://cssreflow.us4.list-manage.com/subscribe/post?u=3de649ceb4c455cc7e4d484eb&amp;id=9238306bb9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" >
+	
+<div class="mc-field-group">
+	<div class="urlBox">
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your_email@your-awesome-site.com"></div>
+</div>
+<div class="mc-field-group">
+	<div class="urlBox">
+	<input type="url" value="" name="MMERGE3" class="required url" id="mce-MMERGE3" placeholder="http://www.your-awesome-site.com"></div>
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>	<div class="clear"><div class="rulesubmit"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button"></div></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+          
+    <br class="clearfloat"><!-- end .content --></div>
+           
+<!-- end #section2 --></div> 
+       
+       
+  <div class="section" id="section3"> 
+		
+		<div class="content">
+        <div class="headline" id="L2headline">Gallery</div>
+        <div class="subheadline" id="L2subheadline">CSS Reflow Visualizations</div>
+        <div class="galBox">
+        <div class="galBoxTitle">html5rocks.com</div>
+    <a class="various fancybox.iframe" href="http://www.youtube.com/embed/fw9jeF14b5g?autoplay=1"><img src="assets/images/html5rocks.jpg" width="400" height="250" alt="html5rocks.com"></a>
+    </div>
+     <div class="galBox"><div class="galBoxTitle">FireFox Download Page</div>
+    <a class="various fancybox.iframe" href="http://www.youtube.com/embed/z9Cps2QEh-k?autoplay=1"><img src="assets/images/ff_dl.jpg" width="400" height="250" alt="FireFox Download Page"></a>
+    </div>
+     <div class="galBox"><div class="galBoxTitle">modernizr.com</div>
+    <a class="various fancybox.iframe" href="http://www.youtube.com/embed/I2MJiJxGQsY?autoplay=1"><img src="assets/images/modernizr.jpg" width="400" height="250" alt="modernizr.com"></a>
+    </div>
+     <div class="galBox"><div class="galBoxTitle">Mozilla Error Page</div>
+    <a class="various fancybox.iframe" href="http://www.youtube.com/embed/x9mc_3x-MpY?autoplay=1"><img src="assets/images/moz_error.jpg" width="400" height="250" alt="Mozilla Error Page"></a>
+    </div>
+    <div class="headline" id="galleryLink"><a href="/gallery.php">view more...</a></div>
+    <br class="clearfloat"><!-- end .content --></div>
+		
+<!-- end #section3 --></div>
+
+<div class="section" id="section4"> 
+
+           <div class="content">
+         <div class="headline" id="L2headline">Contact</div>
+        <div class="subheadline" id="L2subheadline">Reflow Visualization Project</div>
+         
+		 <p class="greeting hw_bi">Greetings!</p>
+		 
+		 <p class="hand_writing greeting">Thanks for your interest in the CSS Reflow Project. We are full time programmers&#44; and when we arent doing client work&#44; we try to dedicate as much time as we can making videos and developing our code. We love the positive response from users all over the globe&#44; and we urge you to keep the URL submissions coming! </p>
+		 
+		 <p class="hand_writing greeting">If you feel so inclined you may make a donation to help keep the project running&#44; by clicking the "Donate" button below. For any questions, or if an enterprising entrepreneur would like to fund the development of this project&#44; feel free to contact me via the email below. </p>
+		 
+		 <p class="greeting hw_bi">Thanks&#44; and have a great day!</p>
+		 
+		 <div class="paypal_form">
+		 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="5MWPWGE2NV36G">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form></div>
+          <a href="mailto:hello@michaelkaminski.me" class="emailLink">hello@michaelkaminski.me</a>
+          
+    <br class="clearfloat"><!-- end .content --></div>
+            
+<!-- end #section4 --></div>
+        <div class="credit">
+<a href="http://bluewebpc.com" target="_blank">robert lullo</a> | <a href="http://michaelkaminski.me" target="_blank">michael kaminski</a>
+</div>
+<br class="clearfloat"><!-- end .container --></div> 
+
+  <script type="text/javascript" src="https://s3.amazonaws.com/pixel_cdn/scripts/scripts.js"></script> 
+  <script type="text/javascript">
+                $(document).ready(function() {
+					$(".various").fancybox({
+						maxWidth	: 800,
+						maxHeight	: 600,
+						fitToView	: false,
+						width		: '70%',
+						height		: '70%',
+						autoSize	: false,
+						closeClick	: false,
+						openEffect	: 'none',
+						closeEffect	: 'none'
+					});
+				});
+        </script>
+  <script type="text/javascript"> 
+	$(document).ready(function(){
+		var lpib_navigation = '<div id="lpib_navigation_wrapper"><div id="lpib_map"><a href="#" id="lpib_map_link">Navigation</a></div><div id="lpib_navigation"><ul><li><a href="#section1" id="lpib_navigation_top">Top</a></li><li><a href="#section2" id="lpib_navigation_howitworks">How it Works</a></li><li><a href="#section3" id="lpib_navigation_pricing">Gallery</a></li><li><a href="#section4" id="lpib_navigation_about">Contact</a></li></ul></div></div>';
+		var lpib_map_left = 0;
+		var lpib_nav_left = -140;
+	
+		$('body').append( lpib_navigation );
+		
+		$('#lpib_map_link').click(function(e){
+			e.preventDefault();
+		});
+		$('#lpib_map_link').mouseenter(function(e){
+			$('#lpib_navigation').animate({
+				left: 0
+			}, 150);
+		});
+		$('#lpib_navigation').mouseleave(function(e){
+			$('#lpib_navigation').animate({
+				left: lpib_nav_left
+			}, 150);
+		});				
+		
+	});
+</script> 
+
+         <script type="text/javascript"> 
+            $(function() {
+                $('ul.nav a, .L1button a, .L2button a, .findout a, #lpib_navigation a, .headline a').bind('click',function(event){
+                    var $anchor = $(this);
+                    
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1500,'easeInOutExpo');
+                    /*
+                    if you don't want to use the easing effects:
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000);
+                    */
+                    event.preventDefault();
+                });
+            });
+        </script> 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24956274-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
