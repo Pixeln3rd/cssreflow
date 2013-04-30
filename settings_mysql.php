0d4f98ee684baa4619f301bd2e273f2dcc5ec40a
Michael Kaminski <hello@michaelkaminski.me>
2013-04-30 13:57:06 -0500

    remove gallery link on homepage
    remove gallery link on homepage. until i get the mysql straightened out
    im removing the gallery page.

--- settings_mysql.php
+++ settings_mysql.php
@@ -1,9 +1,14 @@
 <?php
 
-$mysql_database = "reflow_videos";
-$mysql_user = "user_reflow";
-$mysql_password = "4mXVuWQB7RsPKVeJ";
+$mysql_database = "n3rdc0re_cssreflow";
+$mysql_user = "n3rdc0re_cssuser";
+$mysql_password = "weloveCSS!";
 $mysql_server = "localhost";
+
+//$mysql_database = "reflow_videos";
+//$mysql_user = "user_reflow";
+//$mysql_password = "4mXVuWQB7RsPKVeJ";
+//$mysql_server = "localhost";
 
 
 ?>
